0171871131d6138dbfa51a944763441b3c496346
Mihir Kumar <mihirpandey.13@gmail.com>
2020-12-20 18:27:07 +0530

    small equality bug fixed

--- MERN Stack/backend/src/services/cron.service.js
+++ MERN Stack/backend/src/services/cron.service.js
@@ -7,13 +7,13 @@ module.exports = {
     cronService: async () => {
         try {
             const quote = getQuote();
-            const todos = await Todo.find({completed: false, createdAt: {$gt: new Date(new Date(Date.now()).setHours(00, 00, 00))}});
+            const todos = await Todo.find({ completed: false, createdAt: { $gt: new Date(new Date(Date.now()).setHours(00, 00, 00)) } });
             const recipients = await User.find({});
             if (todos.length === 0) {
                 const mailBody = `<h2>Congratulations</h2>, You are done for the day. <br>
                                     Keep up the same pace.<br>${quote.text} <br> --${quote.author}`;
                 await sendEmail(recipients[0].email, 'Good work: Notification', mailBody);
-            } else if (todos.length >= 0) {
+            } else if (todos.length > 0) {
                 let listOfTasks = '';
                 for (const iterator of todos) {
                     listOfTasks += `${iterator.title}<br>`
